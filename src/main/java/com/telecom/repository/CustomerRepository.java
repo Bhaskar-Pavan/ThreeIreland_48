d2262464872b5bcdb2d9b54ec90b982904d64fe6
bhaskarpavan <kbpsatish2001@gmail.com>
2025-05-02 10:11:05 +0000

    del

--- src/main/java/com/telecom/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.telecom.repository;
-
-import com.telecom.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}

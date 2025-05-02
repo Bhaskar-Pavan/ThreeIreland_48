b7f9bc3945e6bca4971a4d660454896b5f8c4480
bhaskarpavan <kbpsatish2001@gmail.com>
2025-05-02 10:00:30 +0000

    v

--- src/main/routes/sim-order.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth.middleware');
-const simOrderController = require('../controllers/sim-order.controller');
-
-router.post('/', authMiddleware, simOrderController.orderSim);
-
-module.exports = router;
